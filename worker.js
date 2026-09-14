bf4e7af3547c3ccb256b8bba93e60370391b09b2
behradb44-sketch <behradb44@gmail.com>
2026-09-14 02:55:15 -0700

    Update worker.js

--- worker.js
+++ worker.js
@@ -1,1 +1,19 @@
+export default {
+  async fetch(request, env) {
+    const url = new URL(request.url);
 
+    return new Response(
+      JSON.stringify({
+        ok: true,
+        message: "BEHRAD M PLAYER API is online 🚀",
+        path: url.pathname,
+        database: !!env.DB
+      }),
+      {
+        headers: {
+          "Content-Type": "application/json; charset=UTF-8"
+        }
+      }
+    );
+  }
+};
